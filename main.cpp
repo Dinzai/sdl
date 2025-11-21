c2d02b6ea6206483c37bacced41e3467fd1affa0
Brett Rogers <135577737+Dinzai@users.noreply.github.com>
2025-11-21 10:23:54 -0500

    other

--- main.cpp
+++ main.cpp
@@ -3,6 +3,50 @@
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
+int main(int argc, char* argv[])
+{
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Event windowEvent;
+
+    while(true)
+    {
+        if( SDL_PollEvent(&windowEvent))
+        {
+            if(SDL_QUIT == windowEvent.type)
+            {
+                break;
+            }
+        }
+
+
+        SDL_SetRenderDrawColor(renderer, 0,0,0,255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+        SDL_RenderDrawPoint(renderer, 640/2, 480/2);
+
+        SDL_RenderPresent(renderer);
+        
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
+
+
+
+
+
+/*
+const int WIDTH = 800;
+const int HEIGHT = 600;
 
 int main(int argc, char* argv[])
 {
@@ -35,3 +79,5 @@ int main(int argc, char* argv[])
     return EXIT_SUCCESS;
 
 }
+
+*/
